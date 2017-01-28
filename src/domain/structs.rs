5e5696e9fec7f8f81e77458d6ca43ad71bd49ead
Euan Kemp <euank@euank.com>
2017-01-28 01:35:21 -0800

    domain: flesh out more fields

--- src/domain/structs.rs
+++ src/domain/structs.rs
@@ -1,18 +1,44 @@
-use ::xml;
-use ::xml::writer::events::XmlEvent;
+use xml;
+use std::io::Write;
+use xml::writer::events::XmlEvent;
 
 #[derive(PartialEq, Debug)]
-pub struct Domain {
-    pub type_: String,
-    pub id: String,
+pub struct Domain<'a> {
+    // attributes
+    pub type_: &'a str,
+    pub id: Option<i32>,
+
+    // fields
+    pub name: &'a str,
+    pub uuid: &'a str,
+    pub title: Option<&'a str>,
+    pub description: Option<&'a str>,
+    pub metadata: Vec<&'a str>, // TODO, represent better
 }
 
-impl Domain {
+fn write_el<W: Write>(w: &mut xml::writer::EventWriter<W>, name: &str, val: &str) {
+    w.write(XmlEvent::start_element(name));
+    w.write(val);
+    w.write(XmlEvent::end_element());
+}
+
+impl<'a> Domain<'a> {
     pub fn xml(&self) -> String {
         let mut b = Vec::new();
         {
+            let idstr = self.id.map(|e| e.to_string());
+
             let mut w = xml::EmitterConfig::default().perform_indent(true).create_writer(&mut b);
-            w.write(XmlEvent::start_element("domain").attr("type", &self.type_).attr("id", &self.id));
+            let mut domain_ev = XmlEvent::start_element("domain").attr("type", self.type_);
+
+            domain_ev = match idstr {
+                Some(ref id) => domain_ev.attr("id", id),
+                _ => domain_ev,
+            };
+
+            w.write(domain_ev);
+            write_el(&mut w, "name", self.name);
+            write_el(&mut w, "uuid", self.uuid);
             w.write(XmlEvent::end_element());
         }
         String::from_utf8_lossy(&b).into_owned()
@@ -26,10 +52,21 @@ mod test {
 
     #[test]
     fn domain_attributes() {
-        let d = Domain{type_: "xen".to_string(), id: "3".to_string()};
+        let d = Domain {
+            type_: "xen",
+            id: Some(3),
+            name: "bob",
+            uuid: "1234",
+            title: None,
+            description: None,
+            metadata: Vec::new(),
+        };
         let s = r#"
 <?xml version="1.0" encoding="utf-8"?>
-<domain type="xen" id="3" />"#;
+<domain type="xen" id="3">
+  <name>bob</name>
+  <uuid>1234</uuid>
+</domain>"#;
 
         assert_eq!(s.trim(), d.xml().trim());
     }
